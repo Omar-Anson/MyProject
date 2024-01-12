68bd6f0ff7274d7b3559c4f4ad96bbfe8eca7fad
Omar-Anson <oanson@ggc.edu>
2024-01-11 19:23:00 -0500

    first commit

---
+++ src/MySecondProgram.java
@@ -0,0 +1,21 @@
+/**
+ * Class: MySecondProgram
+ * @author Omar Anson
+ * @Date: 01/11/24
+ * @version 1.0
+ * @Course: ITEC 2140 - 03 Spring 2024
+ * Description: How to write the string value
+ */
+
+public class MySecondProgram {
+    public static void main(String [] args){
+        String quote1 = "Oh, Romeo, Romeo ";
+        String quote2 = "   Where are you Romeo?";
+        String full = quote1 + "\n" + quote2;
+        System.out.println(full);
+
+        /**
+         *
+         */
+    }
+}
